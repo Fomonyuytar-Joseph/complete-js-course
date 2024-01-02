ba8454c9a8e6e821e04908eec161cb62dd6b53db
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2024-01-02 22:05:54 +0100

    learned about opertor precedence and did a challenge

---
+++ script.js
@@ -0,0 +1,23 @@
+// console.log('Jonas');
+
+// let firstname = 'Fomonyuytar';
+// console.log(typeof firstname)
+
+let age = 30;
+age =12;
+lastName= 'Joseph'
+
+
+let markWeight = 78;
+let johnWeight = 92;
+
+let markHeight = 1.69;
+let johnHeight = 1.95;
+
+//calculate the BMI
+let markBMI = markWeight / (markHeight ** 2);
+let johnBMI = johnWeight / (johnHeight ** 2);
+
+console.log(markBMI,johnBMI);
+//check if makrj bmi is higher than John 
+console.log(markBMI > johnBMI);
