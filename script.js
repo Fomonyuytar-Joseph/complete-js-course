4b5f2825933059b95c758c841b55a702fbfaa430
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2024-01-03 11:33:43 +0100

    conditionals

--- script.js
+++ script.js
@@ -3,21 +3,41 @@
 // let firstname = 'Fomonyuytar';
 // console.log(typeof firstname)
 
-let age = 30;
-age =12;
-lastName= 'Joseph'
+// let age = 30;
+// age =12;
+// lastName= 'Joseph'
 
 
-let markWeight = 78;
-let johnWeight = 92;
+// let markWeight = 78;
+// let johnWeight = 92;
 
-let markHeight = 1.69;
-let johnHeight = 1.95;
+// let markHeight = 1.69;
+// let johnHeight = 1.95;
 
-//calculate the BMI
-let markBMI = markWeight / (markHeight ** 2);
-let johnBMI = johnWeight / (johnHeight ** 2);
+// //calculate the BMI
+// let markBMI = markWeight / (markHeight ** 2);
+// let johnBMI = johnWeight / (johnHeight ** 2);
 
-console.log(markBMI,johnBMI);
-//check if makrj bmi is higher than John 
-console.log(markBMI > johnBMI);
+// console.log(markBMI,johnBMI);
+// //check if makrj bmi is higher than John 
+// console.log(markBMI > johnBMI);
+
+//template literal
+// let firstname = 'Fomonyuytar';
+// let job = 'engineer';
+// const birthyear = 1990;
+// const year = 2037;
+
+// const joseph = `I'm ${firstname}`
+
+// console.log(joseph);
+
+const age = 15;
+const isOldEnough= age >= 18;
+
+if (isOldEnough) {
+    console.log('Joseph is old enough to drive 🚗');
+}else{
+    const yearsLeft= 18 - age;
+    console.log(`Joseph is too young ,wait another ${yearsLeft} years to drive`);
+}
