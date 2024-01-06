6ca4c959a08f6153255937105bde61053117ca95
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2024-01-06 10:00:03 +0100

    functions

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 'use strict';
 
+function logger (){
+    console.log('My name is Joseph');
+}
+
+
+//calling, running or invoking function
+logger();
+
+
+function fruitProcessor (apples , oranges){
+    console.log(apples , oranges);
+const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+return juice;
+     
+}
+
+const appleJuice =fruitProcessor(5,2);
+console.log(appleJuice);
 
 
 
