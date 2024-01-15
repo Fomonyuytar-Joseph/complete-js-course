5c175cf350ee93094c7f9058600b5ca827845454
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2024-01-15 05:42:08 +0100

    click events

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = '🎊 Correct Number';
-console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 16;
-
-console.log(document.querySelector('.guess').value);
+//select the butto and return element and then add event listener
+document.querySelector('.check').addEventListener('click',function (){
+    console.log(document.querySelector('.guess').value);
+    document.querySelector('.message').textContent = '🎊 Correct Number!';
+})
 
 
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
+
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = '🎊 Correct Number';
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 16;
+
+// console.log(document.querySelector('.guess').value);
+
+
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
