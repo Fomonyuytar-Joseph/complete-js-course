95080230457543e18f58a035f3063a14c1ca7f21
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2024-01-20 10:35:26 +0100

    keypress

--- script.js
+++ script.js
@@ -4,22 +4,30 @@ const overlay = document.querySelector('.overlay');
 const btnCLoseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-const openModal= function(){
- modal.classList.remove('hidden');
- overlay.classList.remove('hidden');
-}
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
-const closeModal= function(){
-   modal.classList.add('hidden');
-   overlay.classList.add('hidden');
-}
-
-console.log(btnsOpenModal);
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener('click',openModal);
+  btnsOpenModal[i].addEventListener('click', openModal);
 }
 
-btnCLoseModal.addEventListener('click', closeModal)
+btnCLoseModal.addEventListener('click', closeModal);
 
-overlay.addEventListener('click',closeModal)
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (event) {
+  //  console.log(event.key);
+
+  if (event.key === 'Escape') {
+    if (!modal.classList.contains('hidden')) {
+      closeModal();
+    }
+  }
+});
