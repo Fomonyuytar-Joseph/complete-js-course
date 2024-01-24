9e4c17ea5a0536f73a67f1f8d947ff2228d97bfb
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2024-01-24 08:19:22 +0100

    enhanced object literal

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 'use strict';
 
-;
+ const openingHours = {
+   thu: {
+     open: 12,
+     close: 22,
+   },
+   fri: {
+     open: 11,
+     close: 23,
+   },
+   sat: {
+     open: 0, // Open 24 hours
+     close: 24,
+   },
+ };
+
+
+
+
 
 
 const restaurant = {
@@ -12,110 +29,31 @@ const restaurant = {
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
+
+
+  //es6 enhanced object literals
+  openingHours,
 
   oderDelivery: function({mainIndex,starterIndex,time,address}){
        console.log(
          `order received ${this.starterMenu[starterIndex]} to ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
        );
   },
-  orderPizza: function(mainIng,...otherIng){
+
+
+  //es6 enhanced object literals to write methods
+  orderPizza(mainIng,...otherIng){
                 console.log(mainIng);
                 console.log(otherIng);
   }
 };
 
-//spread because it is on the right hand side of the = operator
-const arr = [1,2,...[3,4]]
 
-const [a,b,...others]= [1,2,3,4,5];
+ 
 
-console.log(others);
-
-   
-      const [pizza,,risotto,...otherFood]=[...restaurant.mainMenu,...restaurant.starterMenu]
-      console.log(pizza,risotto,otherFood);
+  //three new ways to write objects in es6
 
 
-  //Objects
-  const {sat,...weekdays}= restaurant.openingHours;
-
-  console.log(weekdays);
-   
-//2) Functions
-const add = function(...numbers){
-console.log(numbers);
-let sum = 0;
-for(let num of numbers){
-  sum += num 
-}
-
-console.log(sum);
-}
-
-
-add(2,3)
-add(5,6,7,7,4)
-add(3,5,6,3,2,6,4)
-
-
-restaurant.orderPizza('mushrooms','onions','pizza')
-
-
-
-
-
-
-
-
-
-
-
-
-
-// restaurant.oderDelivery({
-//   time:'22:30',
-//   address:'buea',
-//   mainIndex:2,
-//   starterIndex:2,
-// })
-
-
-
-// const {name ,openingHours,categories} = restaurant;
-
-// const {name:restaurantName,openingHours:hours,categories:tags}=restaurant
-
-// const {menu=[],starterMenu:starters = []}=restaurant
-
-// console.log(menu,starters);
-
-
-// //Mutating variables
-// let a= 111;
-// let b =999;
-
-// const  obj = {a:23,b:27,c:14};
-
-// ({a,b}=obj)
-// console.log(a,b);
-
-// //nested objects
-// const {fri:{open:o,close:c}}=openingHours;
-// console.log(open,close);
 
 
 
