4ee628324090b237a3227fa84dcb46f49775f390
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2024-01-29 10:33:52 +0100

    bankist project

--- script.js
+++ script.js
@@ -36,92 +36,53 @@ const account4 = {
 const accounts = [account1, account2, account3, account4];
 
 // Elements
-// const labelWelcome = document.querySelector('.welcome');
-// const labelDate = document.querySelector('.date');
-// const labelBalance = document.querySelector('.balance__value');
-// const labelSumIn = document.querySelector('.summary__value--in');
-// const labelSumOut = document.querySelector('.summary__value--out');
-// const labelSumInterest = document.querySelector('.summary__value--interest');
-// const labelTimer = document.querySelector('.timer');
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
 
-// const containerApp = document.querySelector('.app');
-// const containerMovements = document.querySelector('.movements');
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
 
-// const btnLogin = document.querySelector('.login__btn');
-// const btnTransfer = document.querySelector('.form__btn--transfer');
-// const btnLoan = document.querySelector('.form__btn--loan');
-// const btnClose = document.querySelector('.form__btn--close');
-// const btnSort = document.querySelector('.btn--sort');
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
 
-// const inputLoginUsername = document.querySelector('.login__input--user');
-// const inputLoginPin = document.querySelector('.login__input--pin');
-// const inputTransferTo = document.querySelector('.form__input--to');
-// const inputTransferAmount = document.querySelector('.form__input--amount');
-// const inputLoanAmount = document.querySelector('.form__input--loan-amount');
-// const inputCloseUsername = document.querySelector('.form__input--user');
-// const inputClosePin = document.querySelector('.form__input--pin');
-
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
-
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-/////////////////////////////////////////////////
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
 
 
 
-// for(const movement of movements){
-//   if(movement>0){
-//     console.log(`you deposited ${movement}`);
-//   }else{
-//        console.log(`you withdrew ${movement}`);
-//   }
-// }
+const displayMovements= function(movements){
+movements.forEach(function(mov,i){
+  const type = mov > 0 ? 'deposit':'withdrawal'
+  // template literals
+  const html = ` <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${
+    i + 1
+  } ${type}</div>
+          <div class="movements__value">${mov}</div>
+        </div>`;
 
 
-
-// for(const [i, movement] of movements.entries()){
-//       if(movement>0){
-//       console.log(`you deposited ${movement}`);
-//     }else{
-//          console.log(`you withdrew ${movement}`);
-//     }
-// }
-
-
-movements.forEach(function(movement,i){
-    if(movement>0){
-    console.log(`you deposited ${i + 1} ${movement}`);
-  }else{
-       console.log(`you withdrew  ${i + 1 } ${movement}`);
-  }
-
+        containerMovements.insertAdjacentHTML('afterbegin',html)
 })
+}
 
 
-//for each method to acheive it also
+displayMovements(account1.movements)
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
 
 
-// let arr = ['a','b','c','d','e']
-
-// console.log(arr.at(0));
-// console.log('joseph'.at(0));
-
-// function filter_list(l) {
-//   let filtered_array = l.filter(item => typeof item === 'number');
-//   return filtered_array
-// }
-
-// const a = 3
-// console.log(typeof a);
-
-
-// console.log(filter_list([1, 'a', 'b', 0, 15]));
-// console.log(filter_list([1, 2, 'aasf', '1', '123', 123]));
