e2f4b9d8bbc110d7ec60fc393677d7b9698fbf93
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2024-03-05 06:38:27 +0100

    numbers

--- script.js
+++ script.js
@@ -2,17 +2,31 @@
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
-//Functions
-//default parameters
-
 // BANKIST APP
 
+/////////////////////////////////////////////////
 // Data
+
+// DIFFERENT DATA! Contains movement dates, currency and locale
+
 // const account1 = {
 //   owner: 'Jonas Schmedtmann',
-//   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+//   movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
 //   interestRate: 1.2, // %
 //   pin: 1111,
+
+//   movementsDates: [
+//     '2019-11-18T21:31:17.178Z',
+//     '2019-12-23T07:42:02.383Z',
+//     '2020-01-28T09:15:04.904Z',
+//     '2020-04-01T10:17:24.185Z',
+//     '2020-05-08T14:11:59.604Z',
+//     '2020-05-27T17:01:17.194Z',
+//     '2020-07-11T23:36:17.929Z',
+//     '2020-07-12T10:51:36.790Z',
+//   ],
+//   currency: 'EUR',
+//   locale: 'pt-PT', // de-DE
 // };
 
 // const account2 = {
@@ -20,24 +34,24 @@
 //   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
 //   interestRate: 1.5,
 //   pin: 2222,
+
+//   movementsDates: [
+//     '2019-11-01T13:15:33.035Z',
+//     '2019-11-30T09:48:16.867Z',
+//     '2019-12-25T06:04:23.907Z',
+//     '2020-01-25T14:18:46.235Z',
+//     '2020-02-05T16:33:06.386Z',
+//     '2020-04-10T14:43:26.374Z',
+//     '2020-06-25T18:49:59.371Z',
+//     '2020-07-26T12:01:20.894Z',
+//   ],
+//   currency: 'USD',
+//   locale: 'en-US',
 // };
 
-// const account3 = {
-//   owner: 'Steven Thomas Williams',
-//   movements: [200, -200, 340, -300, -20, 50, 400, -460],
-//   interestRate: 0.7,
-//   pin: 3333,
-// };
+// const accounts = [account1, account2];
 
-// const account4 = {
-//   owner: 'Sarah Smith',
-//   movements: [430, 1000, 700, 50, 90],
-//   interestRate: 1,
-//   pin: 4444,
-// };
-
-// const accounts = [account1, account2, account3, account4];
-
+// /////////////////////////////////////////////////
 // // Elements
 // const labelWelcome = document.querySelector('.welcome');
 // const labelDate = document.querySelector('.date');
@@ -64,37 +78,33 @@
 // const inputCloseUsername = document.querySelector('.form__input--user');
 // const inputClosePin = document.querySelector('.form__input--pin');
 
+// /////////////////////////////////////////////////
+// // Functions
+
 // const displayMovements = function (movements, sort = false) {
+//   containerMovements.innerHTML = '';
+
 //   const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
 
 //   movs.forEach(function (mov, i) {
 //     const type = mov > 0 ? 'deposit' : 'withdrawal';
-//     // template literals
-//     const html = ` <div class="movements__row">
-//           <div class="movements__type movements__type--${type}">${
+
+//     const html = `
+//       <div class="movements__row">
+//         <div class="movements__type movements__type--${type}">${
 //       i + 1
 //     } ${type}</div>
-//           <div class="movements__value">${mov}</div>
-//         </div>`;
+//         <div class="movements__value">${mov}€</div>
+//       </div>
+//     `;
 
 //     containerMovements.insertAdjacentHTML('afterbegin', html);
 //   });
 // };
 
-// const calcDisplayBalance = acc => {
-//   acc.balance = acc.movements.reduce((acc, cur) => acc + cur, 0);
-
-//   labelBalance.textContent = `${acc.balance} CFA`;
-// };
-
-// const createUsernames = function (accs) {
-//   accs.forEach(function (acc) {
-//     acc.username = acc.owner
-//       .toLowerCase()
-//       .split(' ')
-//       .map(name => name[0])
-//       .join('');
-//   });
+// const calcDisplayBalance = function (acc) {
+//   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
+//   labelBalance.textContent = `${acc.balance}€`;
 // };
 
 // const calcDisplaySummary = function (acc) {
@@ -119,99 +129,112 @@
 //   labelSumInterest.textContent = `${interest}€`;
 // };
 
+// const createUsernames = function (accs) {
+//   accs.forEach(function (acc) {
+//     acc.username = acc.owner
+//       .toLowerCase()
+//       .split(' ')
+//       .map(name => name[0])
+//       .join('');
+//   });
+// };
 // createUsernames(accounts);
 
 // const updateUI = function (acc) {
-//   //display movements
+//   // Display movements
 //   displayMovements(acc.movements);
 
-//   //display balance
+//   // Display balance
 //   calcDisplayBalance(acc);
 
-//   //display summary
+//   // Display summary
 //   calcDisplaySummary(acc);
 // };
 
-// //Event handler
-// let currentAcount;
+// ///////////////////////////////////////
+// // Event handlers
+// let currentAccount;
+
 // btnLogin.addEventListener('click', function (e) {
+//   // Prevent form from submitting
 //   e.preventDefault();
-//   currentAcount = accounts.find(
+
+//   currentAccount = accounts.find(
 //     acc => acc.username === inputLoginUsername.value
 //   );
-//   console.log(currentAcount);
+//   console.log(currentAccount);
 
-//   if (currentAcount?.pin === Number(inputLoginPin.value)) {
-//     labelWelcome.textContent = `Welcome to ${
-//       currentAcount.owner.split(' ')[0]
+//   if (currentAccount?.pin === +(inputLoginPin.value)) {
+//     // Display UI and message
+//     labelWelcome.textContent = `Welcome back, ${
+//       currentAccount.owner.split(' ')[0]
 //     }`;
+//     containerApp.style.opacity = 100;
+
+//     // Clear input fields
+//     inputLoginUsername.value = inputLoginPin.value = '';
+//     inputLoginPin.blur();
+
+//     // Update UI
+//     updateUI(currentAccount);
 //   }
-//   containerApp.style.opacity = 100;
-
-//   //clear input fields
-//   inputLoginUsername.value = inputLoginPin.value = '';
-//   //make the field lose it focus
-//   inputLoginPin.blur();
-
-//   //display movements
-//   displayMovements(currentAcount.movements);
-
-//   //display balance
-//   calcDisplayBalance(currentAcount);
-
-//   //display summary
-//   calcDisplaySummary(currentAcount);
 // });
 
 // btnTransfer.addEventListener('click', function (e) {
 //   e.preventDefault();
-//   const amount = Number(inputTransferAmount.value);
+//   const amount = +(inputTransferAmount.value);
 //   const receiverAcc = accounts.find(
 //     acc => acc.username === inputTransferTo.value
 //   );
-
 //   inputTransferAmount.value = inputTransferTo.value = '';
+
 //   if (
 //     amount > 0 &&
 //     receiverAcc &&
-//     currentAcount.balance >= amount &&
-//     receiverAcc.username !== currentAcount.username
+//     currentAccount.balance >= amount &&
+//     receiverAcc?.username !== currentAccount.username
 //   ) {
-//     currentAcount.movements.push(-amount);
+//     // Doing the transfer
+//     currentAccount.movements.push(-amount);
 //     receiverAcc.movements.push(amount);
 
-//     //update UI
-//     updateUI(currentAcount);
+//     // Update UI
+//     updateUI(currentAccount);
 //   }
 // });
 
 // btnLoan.addEventListener('click', function (e) {
 //   e.preventDefault();
-//   const amount = Number(inputLoanAmount.value);
-//   if (amount > 0 && currentAcount.movements.some(mov => mov >= amount * 0.1)) {
-//     //add movement
-//     currentAcount.movements.push(amount);
-//     //update UI
-//     updateUI(currentAcount);
-//     inputLoanAmount.value = inputLoanAmount.value = '';
+
+//   const amount = +(inputLoanAmount.value);
+
+//   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+//     // Add movement
+//     currentAccount.movements.push(amount);
+
+//     // Update UI
+//     updateUI(currentAccount);
 //   }
+//   inputLoanAmount.value = '';
 // });
 
 // btnClose.addEventListener('click', function (e) {
 //   e.preventDefault();
 
 //   if (
-//     inputCloseUsername.value === currentAcount.username &&
-//     Number(inputClosePin.value) === currentAcount.pin
+//     inputCloseUsername.value === currentAccount.username &&
+//     +(inputClosePin.value) === currentAccount.pin
 //   ) {
-//     let index = accounts.findIndex(
-//       acc => acc.username === currentAcount.username
+//     const index = accounts.findIndex(
+//       acc => acc.username === currentAccount.username
 //     );
+//     console.log(index);
+//     // .indexOf(23)
 
-//     //delete account
+//     // Delete account
 //     accounts.splice(index, 1);
 
-//     //hide UI
+//     // Hide UI
 //     containerApp.style.opacity = 0;
 //   }
 
@@ -219,89 +242,22 @@
 // });
 
 // let sorted = false;
-
 // btnSort.addEventListener('click', function (e) {
 //   e.preventDefault();
-//   displayMovements(currentAcount.movements, !sorted);
+//   displayMovements(currentAccount.movements, !sorted);
 //   sorted = !sorted;
 // });
 
-// console.log(accounts);
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// LECTURES
 
-// const owners = ['Jonas','Zach','Adam','Martha']
-// console.log(owners.sort());
-// console.log(movements);
-//sort array in ascending order(from small to large numbers)
-//return < 0 A will come before B(keep order)
-//return > 0 B will come before A(switch order)
+console.log(Math.max(23,4,56,7))
+console.log(Math.min(23,4,56,7))
 
-//ascending order
-// console.log(movements.sort((a,b)=>{
-// if(a>b){
-//   return 1
-// }
-// if(b>a){
-//   return -1
-// }
-// }));
-// console.log(movements.sort((a,b)=>a-b));
 
-// //descending order
-// console.log(movements.sort((a,b)=>b-a));
 
-// const x = new Array(7);
-// x.fill(1,3,5);
-// console.log(x);
 
-//Array.from
-// const y =Array.from({length:7},()=>1 )
-// console.log(y);
 
-// // _ is a throw a way value
-// const z = Array.from({ length: 7 }, (_,i) => i+1);
-// console.log(z);
-// const greet = function(greeting){
-//     return function(name){
-//         console.log(` ${greeting} ${name}`)
-//     }
-// }
-//
-// const greetHey = greet('Hey')
-// greetHey('Joseph')
-// greet('Good evening')('James')
 
-const camairco = {
-  airline: 'Camairco',
-  iataCode: 'LH',
-  bookings: [],
-  book(flightNum, name) {
-    console.log(
-      `${name} booked seat on ${this.airline} flight ${this.iataCode} ${flightNum} `
-    );
-    this.bookings.push({
-      flight: `${this.iataCode} ${flightNum}`,
-      name,
-    });
-  },
-};
 
-camairco.book(234, 'Joseph');
-
-const turkishairways = {
-  airline: 'TurkishAirways',
-  iataCode: 'TA',
-  bookings: [],
-};
-
-const book = camairco.book;
-book.call(turkishairways, 23, 'Serena Williams');
-console.log(turkishairways);
-
-const bookTA = book.bind(turkishairways);
-bookTA(23454, 'Steven Jones');
-
-const bookTA23 = book.bind(turkishairways, 23);
-bookTA23('Matha Cooper');
